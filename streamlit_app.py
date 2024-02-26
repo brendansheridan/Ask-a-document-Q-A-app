b5be0d76b62f841a64559c7645a13040845620d0
Brendan Sheridan <bsheridandesign@gmail.com>
2024-02-26 08:37:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
